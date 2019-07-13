fe42a94ed4d14e687a755967b92c15a525ae4a1a
SayanS <sayanece2@outlook.com>
2019-07-13 19:54:59 +0530

    Pushing to check latest result dir

--- LaestDir.py
+++ LaestDir.py
@@ -8,7 +8,6 @@ Created on Sat Jul 13 12:05:03 2019
 import os
 import glob
 cwd = os.getcwd()
-print("Current dir::",cwd)
 directory=cwd+"\\Result\\"
 latest_res=max(glob.glob(os.path.join(directory, '*/')), key=os.path.getmtime)
 print(latest_res)
